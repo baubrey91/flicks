31dc8a94deb63fda3020d44cb2ef742807bacb84
Brandon Aubrey <brandonaubrey@Brandons-MacBook-Pro-2.local>
2017-03-29 01:40:08 -0700

    posters loaded

--- flicks/MoviesTableViewController.swift
+++ flicks/MoviesTableViewController.swift
@@ -13,10 +13,14 @@ class MoviesTableViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var moviesArray: [NSDictionary]?;
+    var filteredMoviesArray: [NSDictionary]?
+
+    var imageUrl: NSURL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         callService()
+        filteredMoviesArray = moviesArray
 
         tableView.reloadData()
     }
@@ -27,25 +31,6 @@ class MoviesTableViewController: UIViewController {
     }
     
     func callService() {
-        
-//        let apiKey = "5bf0547b7de4003cc2d3f7365471ee39"
-//        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
-        
-        
-//        getJSON(("https://api.themoviedb.org/3/movie/now_playing?api_key=5bf0547b7de4003cc2d3f7365471ee39"), completionHandler: {
-//            json in DispatchQueue.main.async {
-//                if let movies = json["results"] as? [NSDictionary]{
-//                    self.moviesArray = movies
-//                    self.tableView.reloadData()
-//                } else {
-//                    let ac = UIAlertController(title: "UH OH!", message: "Something went wrong, please check internet connection", preferredStyle: .alert)
-//                    ac.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
-//                    self.present(ac, animated: true, completion: nil)
-//                    print(json)
-//                }
-//            }
-//        })
-//    }
     
         let apiKey = "5bf0547b7de4003cc2d3f7365471ee39"
         let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -60,6 +45,8 @@ class MoviesTableViewController: UIViewController {
                 if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary{//, let results = responseDictionary["title"] as? NSDictionary{
                     print(responseDictionary["results"]!)
                     self.moviesArray = responseDictionary["results"] as? [NSDictionary]
+                    self.filteredMoviesArray = self.moviesArray
+
                     self.tableView.reloadData()
                     print(self.moviesArray)
                 }
@@ -97,11 +84,51 @@ extension MoviesTableViewController: UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let movie = moviesArray?[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as! MovieCell
-        cell.movieTitle.text = movie?["title"] as? String//posts?[indexPath.row]["blog_name"] as! String
+        if let movie = filteredMoviesArray?[indexPath.row]{
+        cell.movieTitle.text = movie["title"] as? String//posts?[indexPath.row]["blog_name"] as! String
+        cell.movieDescription.text = movie["overview"] as? String
+        let baseUrl = "http://image.tmdb.org/t/p/w342"
+        
+        if let posterPath = movie["poster_path"] as? String {
+            
+            imageUrl = NSURL(string: baseUrl + posterPath)
+            fadeInImageRequest(poster: cell.posterImage)
+            }
+        }
         
         return cell
+    }
+    
+    func fadeInImageRequest(poster: UIImageView) {
+        
+        
+        let imageRequest = URLRequest(url: imageUrl as URL)
+        
+        poster.setImageWith(imageRequest as URLRequest, placeholderImage: nil, success: {( imageRequest, imageResponse, image) -> Void in
+            
+            // image response will be nil if image is cached
+            
+            if imageResponse != nil {
+                print("Image was NOT cached, fade in image")
+                poster.alpha = 0.0
+                
+                
+                poster.image = image
+                UIView.animate(withDuration: 2.0, animations: { () -> Void in
+                    poster.alpha = 3.0
+                })
+            } else {
+                print ("Image was cached so just update the image")
+                poster.image = image
+            }
+        },
+                            failure: {(imageRequest, imageResponse, error) -> Void in
+                                
+                                print("Failure to get image")
+                                
+        })
+        
     }
     
 //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
